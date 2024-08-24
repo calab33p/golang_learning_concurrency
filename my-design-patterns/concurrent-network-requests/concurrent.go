c9c103338718b70d1b69769b1db82391eb27c0f0
Tom Palarz <tomek.palarz@gmail.com>
2024-08-24 00:53:47 -0500

    Fixed deadlock... I think.

--- my-design-patterns/concurrent-network-requests/concurrent.go
+++ my-design-patterns/concurrent-network-requests/concurrent.go
@@ -5,7 +5,7 @@ import (
 	"sync"
 )
 
-const NumThreads = 5
+const NumThreads = 1
 
 type RequestResponse struct {
 	input  string
@@ -14,7 +14,7 @@ type RequestResponse struct {
 
 func main() {
 
-	inputs := []string{"Hello", "World", "These", "Are", "My", "inputs"}
+	inputs := []string{"Hello", "World", "These", "Are", "My", "Inputs"}
 	results := ProcessData(inputs)
 
 	for _, res := range results {
@@ -25,64 +25,93 @@ func main() {
 func ProcessData(inputs []string) []RequestResponse {
 	var result []RequestResponse
 
-	lookupChan := make(chan RequestResponse, NumThreads)
-	resultChan := make(chan []RequestResponse, NumThreads)
+	responseChans := make([]chan []RequestResponse, NumThreads)
+	for i := 0; i < NumThreads; i++ {
+		responseChans[i] = make(chan []RequestResponse)
+	}
 
 	var wg sync.WaitGroup
 	wg.Add(NumThreads)
 
 	fmt.Printf("Performing network processing for %d inputs...\n", len(inputs))
 
-	// create goroutines
+	lookupChan := Generator(inputs)
+	resultChan := FanIn(responseChans)
+
+	// fan-out to worker  goroutines
 	for j := 0; j < NumThreads; j++ {
 
 		//log.Debug().Msgf("Creating goroutine %d", j)
 		fmt.Printf("Creating goroutine %d\n", j)
 
-		go NetworkThread(lookupChan, resultChan, &wg)
+		go NetworkThread(lookupChan, responseChans[j], &wg)
 	}
-
-	// send inputs to channel for goroutines to pick up
-	for _, in := range inputs {
-		fmt.Printf("	Adding input %s to lookup channel\n", in)
-		lookupChan <- RequestResponse{input: in, output: ""}
-	}
-	//log.Debug().Msgf("Closing lookup chan of size %d", len(lookupChan))
-	fmt.Printf("Closing lookup chan of size %d\n", len(lookupChan))
-	close(lookupChan)
 
 	//log.Debug().Msg("Starting to read results")
 	//log.Debug().Msgf("Size of result chan: %d", len(resultChan))
 	fmt.Println("Starting to read results")
-	fmt.Printf("Size of result chan: %d\n", len(resultChan))
+	//fmt.Printf("Size of result chan: %d\n", len(resultChan))
 
-	//TODO: close resultChan first and use range instead?
-	for k := 0; k < len(resultChan); k++ {
-		rsp := <-resultChan
-		//log.Debug().Msgf("Got result: &v", rsp)
+	for rsp := range resultChan {
 		fmt.Printf("Got result: %v\n", rsp)
-		for _, r := range rsp {
-			//log.Debug().Msgf("Got result: &s", r)
-			fmt.Printf("Got result: %s\n", r)
-			result = append(result, r)
-		}
+		result = append(result, rsp...)
 	}
 
 	//log.Debug().Msg("Waiting on worker threads")
 	fmt.Println("Waiting on worker threads")
-	wg.Wait()
 
-	//log.Debug().Msg("Closing result chan")
-	fmt.Println("Closing result chan")
-	close(resultChan)
+	//TODO: is this waitgroup necessary anymore?
+	wg.Wait()
 
 	return result
 
 }
 
+func Generator(inputs []string) <-chan RequestResponse {
+	lookupChan := make(chan RequestResponse, NumThreads)
+	//lookupChan := make(chan RequestResponse)
+
+	go func() {
+		// send inputs to channel for goroutines to pick up
+		for _, in := range inputs {
+			fmt.Printf("	Adding input %s to lookup channel\n", in)
+			lookupChan <- RequestResponse{input: in, output: ""}
+		}
+		//log.Debug().Msgf("Closing lookup chan of size %d", len(lookupChan))
+		fmt.Printf("Closing lookup chan of size %d\n", len(lookupChan))
+		close(lookupChan)
+	}()
+	return lookupChan
+
+}
+
+func FanIn(respchans []chan []RequestResponse) <-chan []RequestResponse {
+	resultChan := make(chan []RequestResponse, NumThreads)
+	//resultChan := make(chan []RequestResponse)
+	var wg sync.WaitGroup
+
+	go func() {
+		for _, rc := range respchans {
+			wg.Add(1)
+			go func(c chan []RequestResponse) {
+				for rr := range c {
+					resultChan <- rr
+				}
+				wg.Done()
+			}(rc)
+		}
+		wg.Wait()
+		fmt.Println("Closing result chan")
+		close(resultChan)
+	}()
+
+	return resultChan
+}
+
 func NetworkThread(lc <-chan RequestResponse, rc chan<- []RequestResponse, wg *sync.WaitGroup) {
 
 	defer wg.Done()
+	defer close(rc)
 
 	for rr := range lc {
 
